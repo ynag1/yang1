71c668fc509c7f64e74d14bc604e2e95904c4546
yang <itheima@itcast.cn>
2022-08-04 11:21:06 +0800

    公共部分完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,20 @@
 const baseUrl = 'http://www.liulongbin.top:3007'
-$.ajaxPrefilter((option) => {
-  option.url = baseUrl + option.url
+
+$.ajaxPrefilter(option => {
+	if (option.url.includes('/my/')) {
+		option.headers = {
+			Authorization: localStorage.getItem("token"),
+		}
+	}
+	//在发起真正的ajax请求之前 
+	option.url = baseUrl + option.url
+
+	option.complete = res => {
+		if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+			// 强制跳转到登录页面
+			location.href = '/login.html'
+			//  强制清空 token
+			localStorage.removeItem('token')
+		}
+	}
 })
---
+++ assets/js/index.js
@@ -0,0 +1,47 @@
+//! 获取用户基本信息
+const getUserInfo = () => {
+	$.ajax({
+		type: 'GET',
+		url: '/my/userinfo',
+		data: null,
+		success: res => {
+			const { status, message } = res
+			renderAvatar(res.data)
+		}
+	})
+}
+//! 渲染用户头像
+const renderAvatar = data => {
+	// 获取用户名字
+	let name = data.nickname || data.username
+	// 设置欢迎文本
+	$('#welcome').html('欢迎' + name)
+	// 按需渲染用户头像
+	if (data.user_pic !== null) {
+		$('.layui-nav-img').attr('src', data.user_pic)
+		$(".text-avatar").hide()
+	} else {
+		// 渲染文本头像 隐藏
+		$(".layui-nav-img").hide();
+		//没有头像就首字母大写
+		let firstName = name[0].toUpperCase();
+		//给头像赋值
+		$(".text-avatar").html(firstName);
+	}
+}
+getUserInfo()
+
+//! 退出登录
+$("#dxitBtn").click(() => {
+	layer.confirm(
+		"确定退出登录？",
+		{ icon: 3, title: "提示" },
+		function (index) {
+			// 重新跳转到登录页面
+			location.href = "/login.html";
+			// 清空本地存储里面的 token
+			localStorage.removeItem("token");
+			layer.close(index)
+		}
+	);
+});
